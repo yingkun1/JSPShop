e800f73e1d9c95c308df0920c13c1023de841f3f
yingkun <925712087@qq.com>
2020-06-25 23:26:00 +0800

    图片上传

--- src/online/luffyk/servlet/home/doProductAddServlet.java
+++ src/online/luffyk/servlet/home/doProductAddServlet.java
@@ -1,5 +1,6 @@
 package online.luffyk.servlet.home;
 
+import com.jspsmart.upload.*;
 import online.luffyk.domain.Category;
 import online.luffyk.service.impl.CategoryServiceImpl;
 import org.apache.log4j.Logger;
@@ -15,7 +16,7 @@ public class doProductAddServlet extends HttpServlet {
     private Logger logger = Logger.getLogger(doProductAddServlet.class);
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("utf-8");
         resp.setContentType("text/html;charset=utf-8");
         CategoryServiceImpl categoryService = new CategoryServiceImpl();
@@ -27,6 +28,61 @@ public class doProductAddServlet extends HttpServlet {
         }else{
             logger.debug("没有获取到所有的信息");
         }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+        String product_name = req.getParameter("product_name");
+        String product_price = req.getParameter("product_price");
+        logger.debug("product_name:"+product_name);
+        logger.debug("product_price:"+product_price);
+//        SmartUpload smartUpload = new SmartUpload();
+//        //初始化
+//        smartUpload.initialize(this.getServletConfig(),req,resp);
+//        try {
+//            smartUpload.upload();
+//        } catch (SmartUploadException e) {
+//            e.printStackTrace();
+//        }
+//        Files files = smartUpload.getFiles();
+//        File file = files.getFile(0);
+//        logger.debug("file:"+file);
+//        String fileName = file.getFileName();
+//        String filePathName = file.getFilePathName();
+//        logger.debug("filePathNam:"+filePathName);
+//        logger.debug("fileName:"+fileName);
+//        try {
+//            smartUpload.save("img");
+//        } catch (SmartUploadException e) {
+//            e.printStackTrace();
+//        }
+//        String product_name1 = smartUpload.getRequest().getParameter("product_name");
+//        String product_price1 = smartUpload.getRequest().getParameter("product_price");
+//        logger.debug("product_name1:"+product_name1);
+//        logger.debug("product_price1:"+product_price1);
+        SmartUpload smartUpload = new SmartUpload();
+        smartUpload.initialize(this.getServletConfig(),req,resp);
+        try {
+            smartUpload.upload();
+        } catch (SmartUploadException e) {
+            e.printStackTrace();
+        }
+        Files files = smartUpload.getFiles();
+        File file = files.getFile(0);
+        logger.debug("fileName:"+file.getFileName());
+        logger.debug("filePath:"+file.getFilePathName());
+        try {
+            smartUpload.save("img");
+        } catch (SmartUploadException e) {
+            e.printStackTrace();
+        }
+        Request request = smartUpload.getRequest();
+        String product_name1 = request.getParameter("product_name");
+        String product_price1 = request.getParameter("product_price");
+        logger.debug("product_name1:"+product_name1);
+        logger.debug("product_price1:"+product_price1);
 
     }
 }
